16591d3ce25cbb8d0c1db094a303bd8a6e802118
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-29 17:43:15 -0300

    feat: integrate api and populate boards

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -74,7 +74,7 @@ export default function OrderModal({
             {order.products.map(({ _id, product, quantity }) => (
               <div className="item" key={_id}>
                 <img
-                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  src={product.imagePath}
                   alt={product.name}
                   width={56}
                   height={28.51}
--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { Order } from '../../types';
 import OrdersBoard from '../OrdersBoard';
 
+import { api } from '../../utils';
 import { Container } from './styles';
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    api.get('/orders').then(({ data }) => setOrders(data));
+  }, []);
+
+  const ordersByStatus: Record<string, Order[]> = {
+    waiting: orders.filter((order) => order.status === 'WAITING'),
+    inProduction: orders.filter((order) => order.status === 'IN_PRODUCTION'),
+    done: orders.filter((order) => order.status === 'DONE'),
+  };
+
   return (
     <Container>
-      <OrdersBoard icon="🕑" title="Fila de espera" orders={[]} />
-      <OrdersBoard icon="🧑‍🍳" title="Em preparação" orders={[]} />
-      <OrdersBoard icon="✅" title="Pronto!" orders={[]} />
+      <OrdersBoard
+        icon="🕑"
+        title="Fila de espera"
+        orders={ordersByStatus.waiting}
+      />
+      <OrdersBoard
+        icon="🧑‍🍳"
+        title="Em preparação"
+        orders={ordersByStatus.inProduction}
+      />
+      <OrdersBoard icon="✅" title="Pronto!" orders={ordersByStatus.done} />
     </Container>
   );
 }
